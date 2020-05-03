a3e76e0a485b95e6d3d71ad3af8fc62037deaf28
meghamoree <meghamoree@gmail.com>
2020-05-04 00:23:39 +0530

    Add.Adding UseCase3 of Gambler Simulation.

--- GamblerSimulation.sh
+++ GamblerSimulation.sh
@@ -9,3 +9,25 @@ if [ $Value -eq 1 ]
 	else
 		echo Loss
 fi
+ while [[ $stakesAmount -gt $resignLimit && $stakesAmount -lt $playingGoal ]]
+                do
+                        if [[ $((RANDOM%2)) -eq 1 ]]
+                        then
+                                stakesAmount=$(($stakesAmount+$bet))
+                                        winCount[$i]=$((${winCount[$i]}+1))
+                        else
+                                stakesAmount=$(($stakesAmount-$bet))
+                                        loseCount[$i]=$((${loseCount[$i]}+1))
+                        fi
+               done
+
+		if [ $stakesAmount -ge 150 ]
+		then
+			echo "U have won 50% of stake,u should resign for the day "
+			echo "Stake after winning is "$stake
+		else
+			echo "U have lost 50% of stake,u should resign for the day "
+			echo "Stake after loosing in "$stake
+
+		fi
+

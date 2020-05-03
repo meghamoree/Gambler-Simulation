b9856d9c83cad6df3b290c96edcc350b7c082082
meghamoree <meghamoree@gmail.com>
2020-05-04 01:12:35 +0530

    Add.Adding UseCase4 of Gambler Simulation.

--- GamblerSimulation.sh
+++ GamblerSimulation.sh
@@ -1,33 +1,40 @@
 #!/bin/bash -x
+
 stakesAmount=100
 bet=1
-Value=$(( RANDOM%2 ))
+playingGoal=150
 
+Value=$(( RANDOM%2 ))
 if [ $Value -eq 1 ]
 	then
 		echo Win
 	else
 		echo Loss
 fi
- while [[ $stakesAmount -gt $resignLimit && $stakesAmount -lt $playingGoal ]]
+
+monthDays=20
+		for (( i=1; i<=$monthDays; i++ ))
+		do
+			stakeAmount=100
+                while [[ $stakesAmount -gt $resignLimit && $stakesAmount -lt $playingGoal ]]
                 do
                         if [[ $((RANDOM%2)) -eq 1 ]]
                         then
                                 stakesAmount=$(($stakesAmount+$bet))
+
                                         winCount[$i]=$((${winCount[$i]}+1))
                         else
                                 stakesAmount=$(($stakesAmount-$bet))
                                         loseCount[$i]=$((${loseCount[$i]}+1))
                         fi
-               done
+                 done
 
 		if [ $stakesAmount -ge 150 ]
 		then
 			echo "U have won 50% of stake,u should resign for the day "
 			echo "Stake after winning is "$stake
 		else
-			echo "U have lost 50% of stake,u should resign for the day "
+			echo "U have lost 50% of stake,u should resign for the day"
 			echo "Stake after loosing in "$stake
-
 		fi
-
+		done

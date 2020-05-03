56f59c97969eccde36ef51488ab2ee330fa516ff
meghamoree <meghamoree@gmail.com>
2020-05-03 21:59:09 +0530

    Add.Adding UseCase2 of Flip Coin Simulator.

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-Value=$(( RANDOM%2 ))
-if [ $Value -eq 1 ]
-	then
-		echo Head
-	else
-		echo Tails
-fi
+declare -A dict
+
+echo "Welcome to flipCoin simulator "
+
+read -p "Enter the number of times coin to be flipped " num
+
+for(( i = 0; i < num; i++ ))
+do
+        checkRandom=$((RANDOM%2))
+        if [ $checkRandom -eq 0 ]
+         then
+		echo H
+                dict[heads]=$((${dict[heads]}+1))
+                Hpercentage=$((${dict[heads]}*100/num))
+         else
+
+		echo T
+                dict[tails]=$((${dict[tails]}+1))
+                Tpercentage=$((${dict[tails]}*100/num))
+          fi
+done
+echo HeadsCount is ${dict[heads]}
+echo TailsCount is ${dict[tails]}
+echo  "Singlet outcome is tail percent is $Tpercentage" "head percent is $Hpercentage"

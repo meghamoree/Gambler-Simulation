3e46045f9e09ac1826bbf2f7c797c26edfbb473b
meghamoree <meghamoree@gmail.com>
2020-05-03 21:14:22 +0530

    Add.Adding UseCase7 of ArithmeticSorting.

--- ArithmeticSorting.sh
+++ ArithmeticSorting.sh
@@ -1,13 +1,28 @@
-#!/bin/bash -x 
+#!/bin/bash -x
+ 
 read -p "Enter the a value: " a 
 read -p "Enter the a value: " b 
 read -p "Enter the a value: " c
 
 declare -A UC  
 
-UC[2]=$(( $a + $b * $c ))
-UC[3]=$(( $a * $b + $c ))
-UC[4]=$(( $a + $b / $c ))
-UC[5]=$(( $a % $b + $c )) 
+result1=$(( $a + $b * $c ))
+result2=$(( $a * $b + $c ))
+result3=$(( $a + $b / $c ))
+result4=$(( $a % $b + $c ))
 
-echo  "UseCases" ${UC[@]} 
+UC[1]=$result1
+UC[2]=$result2
+UC[3]=$result3
+UC[4]=$result4
+
+echo "output in array is"
+
+for ((i=1; i<=4; i++))
+do
+Usecase[$i]=${UC[$i]}
+echo ${Usecase[$i]}
+done
+
+echo "output in dictionary is"
+echo  "UseCases" ${UC[@]}

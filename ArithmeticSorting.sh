ca516380d367e721fcd2f859ff76bcfe03a35f4a
meghamoree <meghamoree@gmail.com>
2020-05-03 20:48:33 +0530

    Add.Adding UseCase4 of ArithmeticSorting.

--- ArithmeticSorting.sh
+++ ArithmeticSorting.sh
@@ -4,7 +4,9 @@ read -p "Enter the a value: " b
 read -p "Enter the a value: " c 
 
 UC2=$(( $a + $b * $c ))
-UC3=$(( $a * $b + $c )) 
+UC3=$(( $a * $b + $c ))
+UC4=$(( $a + $b / $c )) 
 
 echo $UC2
 echo $UC3
+echo $UC4

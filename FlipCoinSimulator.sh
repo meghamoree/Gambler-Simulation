3cb2a4775bc99874677e1bee7d0f9c87f451080e
meghamoree <meghamoree@gmail.com>
2020-05-03 23:29:48 +0530

    Add.Adding UseCase3  of Flip Coin Simulator.

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -24,3 +24,51 @@ done
 echo HeadsCount is ${dict[heads]}
 echo TailsCount is ${dict[tails]}
 echo  "Singlet outcome is tail percent is $Tpercentage" "head percent is $Hpercentage"
+
+#doublet
+
+declare -A doubleDict
+
+for (( i = 0; i < num; i++ ))
+
+do
+
+   randomCheck1=$((RANDOM%2))
+   randomCheck2=$((RANDOM%2))
+
+   if (( $randomCheck1 == 0 && $randomCheck2 == 0 ))
+
+   then
+
+      doubleDict[HH]=$((${doubleDict[HH]}+1))
+
+   elif (( $randomCheck1 == 0 && $randomCheck2 == 1 ))
+
+   then
+
+       doubleDict[HT]=$((${doubleDict[HT]}+1))
+
+   elif (( $randomCheck1 == 1 && $randomCheck2 == 0 ))
+
+   then
+                doubleDict[TH]=$((${doubleDict[TH]}+1))
+   else
+                doubleDict[TT]=$((${doubleDict[TT]}+1))
+    fi
+done
+
+echo "doublet answer is"
+
+HHpercent=$(( (${doubleDict[HH]}*100)/$num ))
+THpercent=$(( (${doubleDict[TH]}*100)/$num ))
+HTpercent=$(( (${doubleDict[HT]}*100)/$num ))
+TTpercent=$(( (${doubleDict[TT]}*100)/$num ))
+
+echo "HH came "${doubleDict[HH]}
+echo "percentage of HH is " $HHpercent"%"
+echo "TH came "${doubleDict[TH]}
+echo "percentage of TH is " $THpercent"%"
+echo "HT came "${doubleDict[HT]}
+echo "percentage of HT is " $HTpercent"%"
+echo "TT came "${doubleDict[TT]}
+echo "percentage of TT is " $TTpercent"%"

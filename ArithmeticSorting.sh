d9cf1ae0ec6162f8e48a656148ef496cf481c7ad
meghamoree <meghamoree@gmail.com>
2020-05-03 20:36:07 +0530

    Add.Adding UseCase2 of ArithmeticSorting.

--- ArithmeticSorting.sh
+++ ArithmeticSorting.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -x 
-read -p "Enter the a value: "a 
-read -p "Enter the a value: "b 
-read -p "Enter the a value: "c 
+read -p "Enter the a value: " a 
+read -p "Enter the a value: " b 
+read -p "Enter the a value: " c 
 
-
-
-
+UC2=$(( $a + $b * $c ))
+echo $UC2

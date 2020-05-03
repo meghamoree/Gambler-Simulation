49e2bcee49f66cde774b1f89354bc58ced922ed2
meghamoree <meghamoree@gmail.com>
2020-05-03 23:46:53 +0530

    Add.Adding UseCase4  of Flip Coin Simulator.

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -72,3 +72,115 @@ echo "HT came "${doubleDict[HT]}
 echo "percentage of HT is " $HTpercent"%"
 echo "TT came "${doubleDict[TT]}
 echo "percentage of TT is " $TTpercent"%"
+
+
+
+#triplet
+
+for (( i = 0; i < num; i++ ))
+do
+   randomCheck1=$((RANDOM%2))
+   randomCheck2=$((RANDOM%2))
+   randomCheck3=$((RANDOM%2))
+   if (( $randomCheck1 == 0 && $randomCheck2 == 0 && $randomCheck3 == 0 ))
+   then
+       tripleDict[HHH]=$((${tripleDict[HHH]}+1))
+   elif (( $randomCheck1 == 0 && $randomCheck2 == 0 && $randomCheck3 == 1 ))
+   then
+      tripleDict[HHT]=$((${tripleDict[HHT]}+1))
+   elif (( $randomCheck1 == 0 && $randomCheck2 == 1 && $randomCheck3 == 0 ))
+   then
+      tripleDict[HTH]=$((${tripleDict[HTH]}+1))
+   elif (( $randomCheck1 == 1 && $randomCheck2 == 0 && $randomCheck3 == 0 ))
+        then
+      tripleDict[THH]=$((${tripleDict[THH]}+1))
+        elif (( $randomCheck1 == 1 && $randomCheck2 == 1 && $randomCheck3 == 1 ))
+   then
+      tripleDict[TTT]=$((${tripleDict[TTT]}+1))
+   elif (( $randomCheck1 == 1 && $randomCheck2 == 1 && $randomCheck3 == 0 ))
+   then
+      tripleDict[TTH]=$((${tripleDict[TTH]}+1))
+   elif (( $randomCheck1 == 1 && $randomCheck2 == 0 && $randomCheck3 == 1 ))
+   then
+      tripleDict[THT]=$((${tripleDict[THT]}+1))
+   else
+      tripleDict[HTT]=$((${tripleDict[HTT]}+1))
+   fi
+
+done
+
+HHHpercent=$(( (${tripleDict[HHH]}*100)/$num ))
+THHpercent=$(( (${tripleDict[THH]}*100)/$num ))
+HHTpercent=$(( (${tripleDict[HHT]}*100)/$num ))
+TTTpercent=$(( (${tripleDict[TTT]}*100)/$num ))
+HTHpercent=$(( (${tripleDict[HTH]}*100)/$num ))
+THTpercent=$(( (${tripleDict[THT]}*100)/$num ))
+HTTpercent=$(( (${tripleDict[HTT]}*100)/$num ))
+
+echo "HHH came "${doubleDict[HHH]}
+echo "percentage of HHH is " $HHHpercent"%"
+echo "THH came "${doubleDict[THH]}
+echo "percentage of THH is " $THHpercent"%"
+echo "HHT came "${doubleDict[HHT]}
+echo "percentage of HHT is " $HHTpercent"%"
+echo "TTT came "${doubleDict[TTT]}
+echo "percentage of TTT is " $TTTpercent"%"
+echo "HTH came "${doubleDict[HTH]}
+echo "percentage of HTH is " $HTHpercent"%"
+echo "THT came "${doubleDict[THT]}
+echo "percentage of THT is " $THTpercent"%"
+echo "HTT came "${doubleDict[HTT]}
+echo "percentage of HTT is " $HTTpercent"%"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
